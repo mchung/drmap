d56517079c16c11478ff51f8a9efd572eff50916
Marc Chung <mchung@gmail.com>
2008-11-18 11:57:41 -0700

    Drmap

--- lib/drmap/drmap_enumerable.rb
+++ lib/drmap/drmap_enumerable.rb
@@ -1,10 +1,12 @@
 module Enumerable
 
   def drmap(&block)
+    raise "No data" if self.size == 0
     pool = Drmap::BeanstalkPool.new
 
-    jid = rand(100)
+    jid = Kernel.rand(100)
     each_with_index do |element, idx|
+      # puts "Adding #{jid}, #{element}, #{block.to_ruby}"
       pool.put_job(jid, block.to_ruby, element)
     end
 
